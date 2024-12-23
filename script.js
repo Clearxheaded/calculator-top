382f63291f31d999a277ce0c3a75f71596b4e160
clearxheaded <7777x777x7777@gmail.com>
2024-12-23 11:07:47 -0500

    Add 4 functions to perform calculations

---
+++ script.js
@@ -0,0 +1,27 @@
+const operand1 = 0;
+const operand2 = 0;
+const operator = '';
+
+
+
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+console.log(add(5, 7));
+console.log(subtract(5, 7));
+console.log(multiply(5, 7));
+console.log(divide(5, 7));
